d8f0f4ff83197cbf2a40cb3ab9696932a5477f3b
Alex Noir <Varsash@gmail.com>
2023-02-10 00:55:32 +0300

    Add a way to reverse the buttonmaker.py operation

--- buttonmaker.py
+++ buttonmaker.py
@@ -1,12 +1,14 @@
 import os
 import sys
 
+
 def is_int(s):
     try:
         int(s)
         return True
     except ValueError:
         return False
+
 
 os.chdir(os.path.dirname(__file__))
 
@@ -15,25 +17,33 @@ if len(sys.argv) > 1:
     arg = sys.argv[1]
 
 if arg == "":
-    raise input('No file provided! Please drag char.ini onto this script so it can rename buttons properly.')
+    raise input(
+        'No file provided! Please drag char.ini onto this script so it can rename buttons properly.')
 
 droppedFile = open(arg, "r")
 
+reverse = input(
+    'Rename numbered buttons back to their emotes?').lower() == 'y'
 state = input('Input state (on or off): ').lower()
 
 for line in droppedFile.readlines():
     try:
-        lines = line.rstrip().split(' ')
-        if lines[0] in ('[SoundN]', '[SoundT]'):
+        words = line.rstrip().split(' ')
+        if words[0] in ('[SoundN]', '[SoundT]'):
             break
-        num = lines[0].split('=')[0]
+        num = words[0].split('=')[0]
         if not is_int(num):
             continue
 
         emote = line.split('#')[2]
         print(emote)
         for button in os.listdir():
-            if button.lower() == emote.lower() + '.png':
+            if reverse:
+                if button.lower() == f'button{num}_{state}.png':
+                    os.rename(button, f'{emote.lower()}.png')
+                    print(f'Renamed button {num} back to {emote}')
+                    break
+            elif button.lower() == emote.lower() + '.png':
                 os.rename(button, 'button{}_{}.png'.format(num, state))
                 print('Made a new button for {}'.format(emote))
                 break
@@ -45,4 +55,3 @@ input('Donezo')
 # while F in os.listdir():
 #     os.rename(F, 'button{}_{}.png'.format(i, state))
 #     i+=1
-
