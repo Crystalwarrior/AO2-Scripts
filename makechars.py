51ed07c5f8d923e486e220e31973b55b19fd9c9f
Alex Noir <Varsash@gmail.com>
2024-11-01 04:28:52 +0300

    Update music2yaml to be able to handle nested subfolder categories and generate immediately usable music lists Update makecahrs.py to actually search for char.inis and not make erroneous character entries, plus being able to handle nested subfolders

--- makechars.py
+++ makechars.py
@@ -1,6 +1,29 @@
 import os
+import sys
+
+arg = __file__
+if len(sys.argv) > 1:
+    arg = sys.argv[1]
+
+os.chdir(os.path.dirname(arg))
+
+charlist = []
+
+def recursive_search_ini(dir):
+    for f in dir:
+        if f.is_file():
+            if f.name.lower() == 'char.ini':
+                char = os.path.relpath(os.path.dirname(f.path), start=arg)
+                charlist.append(char.replace("\\", "/"))
+                print(f'Found character "{char}"!')
+        elif f.is_dir():
+            recursive_search_ini(os.scandir(f))
+
+print(f'Scanning "{os.getcwd()}"...')
+recursive_search_ini(os.scandir(os.getcwd()))
 
 File = open("characters.yaml", "w")
+for char in charlist:
+    File.write(f'- {char}\n')
 
-for char in os.listdir():
-    File.write('- {}\n'.format(char))
+input(f"{File.name} has been generated!")
--- music2yaml.py
+++ music2yaml.py
@@ -41,16 +41,22 @@ def check_deps():
             )
 
 
-os.chdir(os.path.dirname(__file__))
 
 arg = ""
 if len(sys.argv) > 1:
     arg = sys.argv[1]
 
+scandir = os.getcwd()
+yaml_name = 'music.yaml'
 droppedFile = None
 if arg != "":
-    droppedFile = open(arg, "r")
+    if os.path.isfile(arg):
+        droppedFile = open(arg, "r")
+    elif os.path.isdir(arg):
+        scandir = arg
+        yaml_name = f'{scandir}.yaml'
 
+os.chdir(os.path.dirname(scandir))
 use_lengths = input(
     'Analyze song lengths and put them in length:? (tinytag module required) (y/n)').lower() == 'y'
 if use_lengths:
@@ -58,9 +64,8 @@ if use_lengths:
     from tinytag import TinyTag
     print('Using tinytag module for song lengths...')
 
-File = open("music.yaml", "w")
-Err = open("music_errors.txt", "w")
-File.write('- use_unique_folder: True #If true, this music will be contained entirely within its own folder e.g. base/music/<yaml name>/*.mp3\n')
+File = open(yaml_name, "w")
+File.write('- use_unique_folder: False #If true, this music will be contained entirely within its own folder e.g. base/music/<yaml name>/*.mp3\n')
 
 
 def set_category(category_name):
@@ -72,10 +77,7 @@ def set_category(category_name):
 
 def add_song(song_name):
     duration = -1
-    print(f'use_lengths is {use_lengths}')
     fname = song_name
-    if current_category != "Unsorted":
-        fname = current_category + "/" + song_name
     if use_lengths:
         tag = TinyTag.get(fname)
         duration = tag.duration
@@ -85,9 +87,10 @@ def add_song(song_name):
     print("Name: {} Length: {}".format(song_name, duration))
 
 
-current_category = ""
 if droppedFile:
     input("Writing .mp3, .ogg, .opus, .wav and .m4a and categorizing them based on file argument to music.yaml. Press ENTER to begin.")
+    current_category = ""
+    Err = None
     for line in droppedFile.readlines():
         try:
             line = line.rstrip()
@@ -99,31 +102,31 @@ if droppedFile:
                         current_category = set_category(line[:-1])
                     add_song(line)
                 except:
+                    if not Err:
+                        Err = open("music_errors.txt", "w")
                     Err.write('Error for {}\n'.format(line))
                     input(
                         "Unable to process song: {}! Press ENTER to continue.".format(line))
         except:
             continue
 else:
-    input("Writing all .mp3, .ogg, .opus, .wav and .m4a present in current folder/subfolders to music.yaml. Any subfolders will be made as new categories. Press ENTER to begin.")
+    input(f"Writing all .mp3, .ogg, .opus, .wav and .m4a present in current folder/subfolders to {yaml_name}. Any subfolders will be made as new categories. Press ENTER to begin.")
 
-    for f in os.scandir(os.getcwd()):
-        if f.is_file():
-            if f.name.lower().startswith('[mod]'):
-                continue
-            if f.name.lower().endswith(('.mp3', '.ogg', '.opus', '.wav', '.m4a')):
-                if current_category != 'Unsorted':
-                    current_category = set_category('Unsorted')
-                add_song(f.name)
-        elif f.is_dir():
-            print('Folder: ' + f.path)
-            for song in os.scandir(f.path):
-                if song.is_file():
-                    if song.name.lower().startswith('[mod]'):
-                        continue
-                    if song.name.lower().endswith(('.mp3', '.ogg', '.opus', '.wav', '.m4a')):
-                        if current_category != f.name:
-                            current_category = set_category(f.name)
-                        add_song(song.name)
+    def recursive_songlist(dir, current_category = ""):
+        for f in os.scandir(dir):
+            if f.is_file():
+                if f.name.lower().startswith('[mod]'):
+                    continue
+                if f.name.lower().endswith(('.mp3', '.ogg', '.opus', '.wav', '.m4a')):
+                    song_path = os.path.relpath(f.path, os.getcwd()).replace("\\", "/")
+                    cat = os.path.relpath(os.path.dirname(f.path), os.getcwd()).replace("\\", " - ")
+                    if current_category != cat and cat != ".":
+                        current_category = set_category(cat)
+                    add_song(song_path)
+            elif f.is_dir():
+                print('Folder: ' + f.path)
+                recursive_songlist(f.path, current_category)
+
+    recursive_songlist(scandir)
 
 input("Done! Press ENTER to exit.")
